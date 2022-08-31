8e07bb5d2596a57cb626bd7328116214d4e7dcdf
Bheem Mastkar <111329157+bheem2510@users.noreply.github.com>
2022-08-31 12:25:37 +0530

    Add files via upload

---
+++ pricision.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	cout<<"Pricision set to 3 digits \n\n: ";
+	cout.precision(3);
+	
+	cout.width(10);
+	cout<<"Value";
+	cout.width(15);
+	cout<<"SQRT of value"<< " \n";
+	
+	for(int i=1;i<=5;i++){
+		cout.width(8);
+		cout<<i;
+		cout.width(13);
+		cout<<sqrt(i)<<"\n";
+	}
+		cout<<"\n Precision set to 5 digits \n\n";
+		cout.precision(5);
+		cout<<" sqrt(10) = "<<sqrt(10)<<endl<<endl;
+		cout.precision(0);
+		cout<<" sqrt(10) = "<<sqrt(10)<<"(Default setting)\n"; 
+			 
+	return 0;
+}
